45397113395d5cd5185c113c5c8eedab243409cb
“Bryan <bdscheiderer@gmail.com>
2025-06-19 20:10:49 -0500

    typos fixed

--- ytaudio.py
+++ ytaudio.py
@@ -1,10 +1,11 @@
-# This program download video and audio from a Youtube video
+# This program downloads video and audio from a Youtube video
 
 import sys
+import time
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
 
-# option to hard code download path
+# option to hard code download path (e.g., a music folder)
 # if PATH = None, will dl to same directory as program is run from
 # PATH = "your/download/path"
 PATH = None
@@ -12,7 +13,7 @@ PATH = None
 
 def main():
 
-    # get user input for youtube video url
+    # get user input for YouYube video url
     url =  input("Enter Youtube url:")
 
     # verify url entered 
@@ -22,13 +23,13 @@ def main():
     try:
         yt = YouTube(url, on_progress_callback=on_progress)
     except:
-        print(f'Error: video {url} is unavaialable.')
+        print(f'Error: video {url} is unavailable.')
         sys.exit(0)
     else:
         print(f"Video found: {yt.title}")
 
     # ask if should download video and audio ("y") 
-    # default "n" will downlonad audio file only
+    # default "n" will download audio file only
     try:
         message = "Download video along with audio? (y/n) "
         video_dl = (input(message)).lower()
@@ -37,19 +38,20 @@ def main():
         
     # option to download video...first resolution stream
     if video_dl == "y":
-        print("Downloading Video...")
+        print("Downloading video...")
         yt.streams.first().download(output_path=PATH)
         print()
 
-    # download audio (currently .m4a file)
-    print(f"Downloading Audio...")
+    # download audio (currently a .m4a file)
+    print(f"Downloading audio...")
     ys = yt.streams.get_audio_only()
     ys.download(output_path=PATH)
     print()
 
     # print download path and program complete
     print(f"Download path: {PATH}")
-    print(f"Program Completed")
+    print(f"Program completed.")
+    time.sleep(1)
 
 
 if __name__ == "__main__":
